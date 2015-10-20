ad899e3a67f1320ca834c36dec54b136ee9a8a54
Kęstutis Triponis <kestutis.triponis@gmail.com>
2015-10-20 12:10:59 +0300

    Add route suggestion controller

---
+++ src/Nfq/RidePlanBundle/Controller/RouteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nfq\RidePlanBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RouteController extends Controller
+{
+    public function SuggestAction()
+    {
+        return $this->render('RidePlanBundle:Route:Suggest.html.twig', array());
+    }
+}
---
+++ src/Nfq/RidePlanBundle/Tests/Controller/RouteControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nfq\RidePlanBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RouteControllerTest extends WebTestCase
+{
+}
