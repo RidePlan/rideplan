de9079b297314e46990f703c5f6ef18bceed2fbf
rjonaitis <richard.jonaitis@gmail.com>
2015-10-27 10:19:12 +0200

    added user login form, security patterns

--- src/Nfq/RidePlanBundle/Controller/AccountController.php
+++ src/Nfq/RidePlanBundle/Controller/AccountController.php
@@ -3,9 +3,36 @@
 namespace Nfq\RidePlanBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AccountController extends Controller
 {
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'RidePlanBundle:Account:Login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+
     public function ShowProfileAction()
     {
         return $this->render('RidePlanBundle:Account:ShowProfile.html.twig', array(
